ab98b285136c5abfe11f67954697fef33bc40994
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-05-19 21:36:12 +0100

    Añadiendo schema de libro

---
+++ app/models/books_db.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+// define the schema for our user model
+var bookSchema = mongoose.Schema({
+  
+  title: String,
+  author: String,
+  publisher: String,
+  isbn: Number,
+  price: Number,
+  genre: String,
+  pages: Number,
+  buyDate: { type: Date, default: Date.now },
+  loan: { state: Boolean, loanDate: Date, borrower: String },
+  read: Boolean,
+  owner:[ {type: Schema.Types.ObjectId, ref: 'User'} ]
+});
+  
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Book', bookSchema);
