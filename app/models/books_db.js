2283c7f01cd8cb0189d50271c2f0247498722ca4
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-05-21 18:27:59 +0100

    Solucionando bug en server.js

--- app/models/books_db.js
+++ app/models/books_db.js
@@ -1,17 +1,5 @@
 var mongoose = require('mongoose');
 
-var uri = 'mongodb://heroku_bmtvx0gk:2l9utvibhnq3i5dm8i0roc3vr@ds055855.mlab.com:55855/heroku_bmtvx0gk';
-
-mongoose.Promise = global.Promise
-
-mongoose.connect(uri);
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function callback () {
-
 // define the schema for our user model
 var bookSchema = mongoose.Schema({
 
@@ -41,4 +29,3 @@ var bookSchema = mongoose.Schema({
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Book', bookSchema);
-});
